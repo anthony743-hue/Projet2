fda8e58e3c45d2037c2bc1abb602cdde09b199cc
faniry74 <faniryanthony74@gmail.com>
2026-06-25 09:29:30 +0300

    Ajout de modif

--- app/src/main/java/controllers/BackController.java
+++ app/src/main/java/controllers/BackController.java
@@ -1,8 +1,17 @@
 package main.java.controllers;
 
 import annotation.Controller;
+import annotation.UrlMapping;
 
 @Controller
 public class BackController {
-    
+    @UrlMapping(path = "/alert/test")
+    public void bonjour(){
+
+    }
+
+    @UrlMapping(path = "/alert/list")
+    public void wesh(String s){
+
+    }
 }
--- app/src/main/java/controllers/FrontController.java
+++ app/src/main/java/controllers/FrontController.java
@@ -1,8 +1,18 @@
 package main.java.controllers;
 
 import annotation.Controller;
+import annotation.UrlMapping;
 
 @Controller
 public class FrontController {
     
+    @UrlMapping(path = "/emp/test")
+    public void test(){
+
+    }
+
+    @UrlMapping(path = "/emp/list")
+    public void testMaster(String s){
+
+    }
 }
